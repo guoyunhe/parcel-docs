ab3b2a7e8f7a2784b99ec740ba76be76da99df49
Guo Yunhe <i@guoyunhe.me>
2023-03-17 22:54:47 +0800

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# @guoyunhe/create-react-lib
+
+Create React libraries
+
+```bash
+npm create @guoyunhe/react-lib my-react-lib
+```
+
+See also https://github.com/guoyunhe/react-lib-scripts
---
+++ src/index.ts
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+import { createProject } from '..';
+
+const program = new Command('create-react-lib');
+
+program
+  .argument(
+    '[project]',
+    'Folder name for the created project. If not provided, use current folder name.'
+  )
+  .option(
+    '--package-version <version>',
+    'Initial package version number, 1.0.0 by default'
+  )
+  .action(createProject);
+
+program.helpOption('-h, --help', 'Show full help');
+
+program.version('1.0.0', '-v, --version', 'Show version number');
+
+program.parse();
