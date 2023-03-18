7efc5d6d8060a1ff17d942f7d0e0c2c21d2cc4bc
Guo Yunhe <i@guoyunhe.me>
2023-03-18 10:44:56 +0800

    build docs

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,28 @@
 #!/usr/bin/env node
 
+import { Parcel } from '@parcel/core';
 import { Command } from 'commander';
+import { generate } from './generators/generate';
 
 const program = new Command('create-react-lib');
 
-program
-  .option(
-    '--package-version <version>',
-    'Initial package version number, 1.0.0 by default'
-  )
-  .action(() => {
-    console.log('hi');
+program.action(async () => {
+  await generate();
+  let bundler = new Parcel({
+    entries: '.parcel-docs/index.html',
+    defaultConfig: '@parcel/config-default',
+    defaultTargetOptions: {
+      distDir: '.parcel-docs/dist',
+    },
+    serveOptions: {
+      port: 3000,
+    },
+    hmrOptions: {
+      port: 3000,
+    },
   });
+  await bundler.watch();
+});
 
 program.helpOption('-h, --help', 'Show full help');
 
